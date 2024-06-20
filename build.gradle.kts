995679ef1736a666892c71b4de7c5794ce2bc49c
jade ko <dev.dkko@gmail.com>
2024-06-20 14:04:07 +0900

    chore: queryDSL 설정

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,9 +1,12 @@
 plugins {
 	id("org.springframework.boot") version "3.3.0"
 	id("io.spring.dependency-management") version "1.1.5"
+
 	kotlin("plugin.jpa") version "1.9.24"
 	kotlin("jvm") version "1.9.24"
 	kotlin("plugin.spring") version "1.9.24"
+
+	kotlin("kapt") version "1.9.24"
 }
 
 group = "com.musinsa"
@@ -12,6 +15,12 @@ version = "0.0.1-SNAPSHOT"
 java {
 	toolchain {
 		languageVersion = JavaLanguageVersion.of(21)
+	}
+}
+
+configurations {
+	compileOnly {
+		extendsFrom(configurations.annotationProcessor.get())
 	}
 }
 
@@ -24,6 +33,12 @@ dependencies {
 	implementation("org.springframework.boot:spring-boot-starter-web")
 	implementation("com.fasterxml.jackson.module:jackson-module-kotlin")
 	implementation("org.jetbrains.kotlin:kotlin-reflect")
+
+	runtimeOnly("com.h2database:h2")
+	implementation("com.querydsl:querydsl-jpa:5.0.0:jakarta")
+	kapt("com.querydsl:querydsl-apt:5.0.0:jakarta")
+	kapt("jakarta.annotation:jakarta.annotation-api")
+	kapt("jakarta.persistence:jakarta.persistence-api")
 
 	testImplementation("org.springframework.boot:spring-boot-starter-test")
 	testImplementation("org.jetbrains.kotlin:kotlin-test-junit5")
@@ -40,4 +55,26 @@ kotlin {
 
 tasks.withType<Test> {
 	useJUnitPlatform()
+}
+
+val generated = file("src/main/generated")
+
+tasks.withType<JavaCompile> {
+	options.generatedSourceOutputDirectory.set(generated)
+}
+
+sourceSets {
+	main {
+		kotlin.srcDirs += generated
+	}
+}
+
+tasks.named("clean") {
+	doLast {
+		generated.deleteRecursively()
+	}
+}
+
+kapt {
+	generateStubs = true
 }
