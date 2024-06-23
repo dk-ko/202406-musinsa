786425321e2fcac913df26a71e3429796fb3c01c
jade ko <dev.dkko@gmail.com>
2024-06-23 09:42:46 +0900

    refactor: indent 변경

--- src/main/kotlin/com/musinsa/shop/category/adapter/in/web/CategoryController.kt
+++ src/main/kotlin/com/musinsa/shop/category/adapter/in/web/CategoryController.kt
@@ -3,6 +3,7 @@ package com.musinsa.shop.category.adapter.`in`.web
 import com.musinsa.shop.category.application.port.`in`.CategoryUseCase
 import org.springframework.http.HttpStatus
 import org.springframework.http.ResponseEntity
+import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.PostMapping
 import org.springframework.web.bind.annotation.RequestBody
 import org.springframework.web.bind.annotation.RequestMapping
@@ -15,8 +16,8 @@ class CategoryController(
 ) {
     @PostMapping
     fun createRootCategory(
-        @RequestBody categoryCreateReqDto: CategoryCreateReqDto)
-    : ResponseEntity<CategoryResDto> {
+        @RequestBody categoryCreateReqDto: CategoryCreateReqDto
+    ): ResponseEntity<CategoryResDto> {
         return ResponseEntity(categoryUseCase.createRootCategory(categoryCreateReqDto.name), HttpStatus.CREATED)
     }
 
@@ -24,10 +25,13 @@ class CategoryController(
     fun addSubCategory(
         @RequestBody categoryAddReqDto: CategoryAddReqDto
     ): ResponseEntity<CategoryResDto> {
-        return ResponseEntity(categoryUseCase.addSubCategory(
-            parentCategoryId = categoryAddReqDto.parentCategoryId,
-            subCategoryName = categoryAddReqDto.name,
-        ), HttpStatus.CREATED)
+        return ResponseEntity(
+            categoryUseCase.addSubCategory(
+                parentCategoryId = categoryAddReqDto.parentCategoryId,
+                subCategoryName = categoryAddReqDto.name
+            ),
+            HttpStatus.CREATED
+        )
     }
     // todo select들 API로 빼기
 }
