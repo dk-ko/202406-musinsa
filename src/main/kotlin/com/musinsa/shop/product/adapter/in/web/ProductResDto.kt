dbb0e487e52b596c955eda2ecb187d521bc29025
jade ko <dev.dkko@gmail.com>
2024-06-24 22:58:14 +0900

    refactor: 불필요한 어노테이션 삭제

--- src/main/kotlin/com/musinsa/shop/product/adapter/in/web/ProductResDto.kt
+++ src/main/kotlin/com/musinsa/shop/product/adapter/in/web/ProductResDto.kt
@@ -1,14 +1,8 @@
 package com.musinsa.shop.product.adapter.`in`.web
 
-import org.jetbrains.annotations.NotNull
-
 class ProductResDto(
-    @NotNull
     val id: Long,
-    @NotNull
     val name: String,
-    @NotNull
     val price: Int,
-    @NotNull
     val brandCode: String,
 )
