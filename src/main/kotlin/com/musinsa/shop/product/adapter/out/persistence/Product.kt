91658099310ba4ee54c5688c4e4b242b70694d62
jade ko <dev.dkko@gmail.com>
2024-06-24 20:47:26 +0900

    chore: 불필요한 import 삭제

--- src/main/kotlin/com/musinsa/shop/product/adapter/out/persistence/Product.kt
+++ src/main/kotlin/com/musinsa/shop/product/adapter/out/persistence/Product.kt
@@ -1,9 +1,7 @@
 package com.musinsa.shop.product.adapter.out.persistence
 
 import com.musinsa.shop.brand.adapter.out.persistence.Brand
-import com.musinsa.shop.category.adapter.out.persistence.Category
 import com.musinsa.shop.common.BaseEntity
-import com.musinsa.shop.category.adapter.out.persistence.CategoryProductMapping
 import com.musinsa.shop.product.adapter.`in`.web.ProductResDto
 import jakarta.persistence.*
 
